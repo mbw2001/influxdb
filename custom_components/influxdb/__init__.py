08f8325d2144469791cd639fbcd5cb7ce0ed61c4
Michael <mbw2001@users.noreply.github.com>
2021-11-13 14:40:49 +0100

    Update __init__.py

--- custom_components/influxdb/__init__.py
+++ custom_components/influxdb/__init__.py
@@ -1,1 +1,39 @@
 """ Custom influxdb component """
+import logging
+
+from homeassistant.config_entries import ConfigEntry, ConfigType, SOURCE_IMPORT
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = ["sensor"]
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Configure Open Hardware Monitor using config flow only."""
+    if DOMAIN in config:
+        for entry in config[DOMAIN]:
+            hass.async_create_task(
+                hass.config_entries.flow.async_init(
+                    DOMAIN, context={"source": SOURCE_IMPORT}, data=entry
+                )
+            )
+
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Configure Open Hardware Monitor from config entry."""
+    client = OpenHardwareMonitor(hass, config_entry)
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = client
+    if not await client.async_setup():
+        return False
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+    return unload_ok
