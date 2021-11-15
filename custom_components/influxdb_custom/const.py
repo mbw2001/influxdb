deb857515eea4ebd86e13a4a7a861217dde61a26
Michael <mbw2001@users.noreply.github.com>
2021-11-15 08:40:42 +0100

    Create const.py

---
+++ custom_components/influxdb_custom/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "influxdb_custom"
+
+DEFAULT_HOST = 'influxdb'
+DEFAULT_PORT = 8086
+DEFAULT_SSL = False
+DEFAULT_SCAN_INTERVAL = 15
+
+DATA_UPDATED = "influxdb_data_updated"
+
+CONF_ORG = "organization"
