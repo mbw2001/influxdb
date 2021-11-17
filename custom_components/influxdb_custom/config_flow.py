2504b0d4bdcae2cacee5ab504add1b1a88d7854e
Michael <mbw2001@users.noreply.github.com>
2021-11-17 11:48:48 +0100

    Update config_flow.py

--- custom_components/influxdb_custom/config_flow.py
+++ custom_components/influxdb_custom/config_flow.py
@@ -5,17 +5,10 @@ import voluptuous as vol
 from influxdb_client import InfluxDBClient
 
 from homeassistant.config_entries import ConfigFlow, OptionsFlow
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PORT,
-    CONF_SSL,
-    CONF_TOKEN,
-    CONF_SCAN_INTERVAL
-)
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SSL, CONF_TOKEN, CONF_NAME, CONF_SCAN_INTERVAL
 from homeassistant.core import callback
-from homeassistant.exceptions import HomeAssistantError
 
-from .const import DOMAIN, DEFAULT_HOST, DEFAULT_PORT, DEFAULT_SSL, DEFAULT_SCAN_INTERVAL, CONF_ORG
+from .const import DOMAIN, DEFAULT_HOST, DEFAULT_PORT, DEFAULT_SSL, DEFAULT_NAME, DEFAULT_QUERY, DEFAULT_SCAN_INTERVAL, CONF_ORG, CONF_QUERY
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,9 +18,12 @@ DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_PORT, default=DEFAULT_PORT): vol.Coerce(int),
         vol.Required(CONF_SSL, default=DEFAULT_SSL): bool,
         vol.Required(CONF_TOKEN): str,
-        vol.Required(CONF_ORG): str
+        vol.Required(CONF_ORG): str,
+        vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+        vol.Required(CONF_QUERY, default=DEFAULT_QUERY): str
     }
 )
+
 
 class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     """Handle an InfluxDB config flow."""
@@ -43,30 +39,32 @@ class ConfigFlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-    
+
         if user_input is not None:
-            self._async_abort_entries_match({CONF_HOST: user_input[CONF_HOST]})
+            self._async_abort_entries_match({CONF_NAME: user_input[CONF_NAME]})
 
             """Validate the user input allows us to connect."""
             url = f"http{'s' if user_input[CONF_SSL] else ''}://{user_input[CONF_HOST]}:{user_input[CONF_PORT]}"
             token = user_input[CONF_TOKEN]
             org = user_input[CONF_ORG]
-
+            
             try:
-                client = InfluxDBClient(url=url, token=token, org=org)
+                client = InfluxDBClient(
+                    url=url, token=token, org=org)
                 await self.hass.async_add_executor_job(client.ready)
                 return self.async_create_entry(
-                    title=user_input[CONF_HOST],
+                    title=user_input[CONF_NAME],
                     data=user_input
                 )
             except Exception as e:
-                 errors["base"] = "cannot_connect"
+                errors["base"] = "cannot_connect"
 
         return self.async_show_form(
             step_id="user",
             data_schema=DATA_SCHEMA,
             errors=errors,
         )
+
 
 class OptionsFlowHandler(OptionsFlow):
     """Handle InfluxDB options."""
