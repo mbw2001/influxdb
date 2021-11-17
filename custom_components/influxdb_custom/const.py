86d8946db46e259719f42d4cf1939f56393d6112
Michael <mbw2001@users.noreply.github.com>
2021-11-17 11:49:00 +0100

    Update const.py

--- custom_components/influxdb_custom/const.py
+++ custom_components/influxdb_custom/const.py
@@ -4,7 +4,8 @@ DEFAULT_HOST = 'influxdb'
 DEFAULT_PORT = 8086
 DEFAULT_SSL = False
 DEFAULT_SCAN_INTERVAL = 15
-
-DATA_UPDATED = "influxdb_data_updated"
+DEFAULT_NAME = 'Queries'
+DEFAULT_QUERY = ''
 
 CONF_ORG = "organization"
+CONF_QUERY = "query"
