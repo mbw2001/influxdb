bdbe80831f3b0e0a3ab7689b30cf78bc5efecef0
Michael <mbw2001@users.noreply.github.com>
2021-11-17 11:48:37 +0100

    Update __init__.py

--- custom_components/influxdb_custom/__init__.py
+++ custom_components/influxdb_custom/__init__.py
@@ -2,16 +2,17 @@
 import logging
 
 from homeassistant.config_entries import ConfigEntry, ConfigType, SOURCE_IMPORT
-from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SSL, CONF_TOKEN, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SSL, CONF_TOKEN
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
-from .const import DOMAIN, CONF_ORG
+from .const import DOMAIN, CONF_QUERY, CONF_ORG
 
 from influxdb_client import InfluxDBClient
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["sensor"]
+
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Configure InfluxDB using config flow only."""
@@ -24,19 +25,28 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             )
     return True
 
+
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Configure InfluxDB from config entry."""
     try:
         url = f"http{'s' if config_entry.data[CONF_SSL] else ''}://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}"
-        token = config_entry.data[CONF_TOKEN]
-        org = config_entry.data[CONF_ORG]
-        client = InfluxDBClient(url=url, token=token, org=org)
+
+        client = CustomInfluxDBClient(
+            hass=hass,
+            url=url,
+            token=config_entry.data[CONF_TOKEN],
+            org=config_entry.data[CONF_ORG],
+            query=config_entry.data[CONF_QUERY])
+
         hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = client
         hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+
         return True
+
     except Exception as err:
         _LOGGER.debug("Can not connect to InfluxDB")
         raise ConfigEntryNotReady from err
+
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Unload InfluxDB config entry."""
@@ -44,3 +54,31 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     if unload_ok:
         hass.data[DOMAIN].pop(config_entry.entry_id)
     return unload_ok
+
+
+class CustomInfluxDBClient(InfluxDBClient):
+    def __init__(self, hass: HomeAssistant, url: str, token: str, org: str, query: str):
+        super().__init__(url=url, token=token, org=org)
+        self.hass = hass
+        self.api = self.query_api()
+        self.query = query
+        _LOGGER.debug(f"Running query: {self.query}")
+        self.async_update()
+        self.devices = [('Test', 1, '')]
+
+    async def get_value(self, i: int):
+        _LOGGER.debug(f"Getting value for: {i}")
+        return self.devices[i][1]
+
+    def update(self):
+        _LOGGER.debug(f"Running query: {self.query}")
+        tables = self.api.query(self.query)
+        self.devices = [(
+            tables[0].records[0].values['result'],
+            tables[0].records[0].values['_value'],
+            ''
+        )]
+
+    async def async_update(self):
+        _LOGGER.debug(f"Running query: {self.query}")
+        return await self.hass.async_add_executor_job(self.update)
